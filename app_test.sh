81760c6a3d53756b2839b3fdff845ed39bcba76a
mohammad mazarei <mohammad.mazarei@gmail.com>
2022-04-08 22:51:16 +0430

    add sni test

--- app_test.sh
+++ app_test.sh
@@ -20,7 +20,7 @@ function stop_socks ()
 function start_zorxy ()
 {
     zroxy_path=$1
-    nohup $zroxy_path -d 0.0.0.0:53000 -u8.8.8.8 -s127.0.0.1:58080 > /tmp/zroxy.txt &
+    nohup $zroxy_path -p127.0.0.1:4433#443 -d 0.0.0.0:53000 -u8.8.8.8 -s127.0.0.1:58080 > /tmp/zroxy.txt &
     echo $!
 }
 
@@ -31,8 +31,8 @@ function stop_zroxy ()
 
 function domain_test ()
 {
-    org_ip=$(dig $1 A @8.8.8.8 +short -p53 +tcp | head -n 1)
-    zro_ip=$(dig $1 A @127.0.0.1 +short -p53000  | head -n 1)
+    org_ip=$(dig $1 A @8.8.8.8 +short -p53 +tcp +time=5 +tries=1 | head -n 1)
+    zro_ip=$(dig $1 A @127.0.0.1 +short -p53000  +time=5 +tries=1 | head -n 1)
     if [ "$org_ip" = "$zro_ip" ]; then
         echo "pass"
     else
@@ -53,6 +53,31 @@ function dns_test ()
     done
 }
 
+function sni_domain_test ()
+{
+    zro_con=$(curl --silent https://$1:4433 --resolve $1:4433:127.0.0.1 -H "Host: $1"| sha512sum)
+    org_con=$(curl --silent https://$1 | sha512sum)
+    if [ "$zro_con" = "$org_con" ]; then
+        echo "pass"
+    else
+        echo "fail"
+    fi
+}
+
+function sni_test ()
+{
+    domains=("ping.eu" "www.arngren.net" "google.com" "www.pacificgrandprix.com" "edition.cnn.com")
+    for domain in ${domains[@]}; do
+        res=$(sni_domain_test $domain)
+        if [ "$res" = "pass" ]; then
+            echo -e "SNI $C_YELLOW$domain$C_NoColor Test $C_GREEN$res$C_NoColor" >&2
+        else
+            echo -e "SNI $C_YELLOW$domain$C_NoColor Test $C_RED$res$C_NoColor" >&2    
+        fi
+    done
+}
+
+
 zroxy=$1
 echo "---- Start app test ----"
 
@@ -68,9 +93,11 @@ sleep 0.5
 echo -e "\ndsn test..."
 res=$(dns_test)
 
+echo -e "\nSNI test..."
+res=$(sni_test)
 
 #stop zroxy
-echo "stop zroxy"
+echo -e "\nstop zroxy"
 stop_zroxy $zroxy_pid
 
 #stop socks proxy
